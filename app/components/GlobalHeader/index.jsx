c6de9a4b3f462cfa822efa1d91219bb727b13dde
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2019-01-26 15:25:08 -0500

    add search inoput in glonal header

--- app/components/GlobalHeader/index.jsx
+++ app/components/GlobalHeader/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Layout, Row, Col, Dropdown, Button, Menu } from 'antd'
+import { Layout, Row, Col, Dropdown, Button, Menu, Input } from 'antd'
 import { Icon } from 'react-fa'
 import logo from '../../assets/logo.png'
 import './style.scss'
 import AvatarButton from '../AvatarButton'
 
 const { Header } = Layout
+const { Search } = Input
 
 class GlobalHeader extends React.Component {
   constructor(props) {
@@ -19,7 +20,14 @@ class GlobalHeader extends React.Component {
           <Col span={8} className="global-header__left">
             <img className="global-header__left__logo" src={logo} alt="magzeen" />
           </Col>
-          <Col span={8} offset={8} className="global-header__right">
+          <Col span={8} className="global-header__center">
+            <Search
+              placeholder="Search posts..."
+              onSearch={value => console.log(value)}
+              className="global-header__center__search"
+            />
+          </Col>
+          <Col span={8} className="global-header__right">
             <AvatarButton />
             <div className="global-header__right__notification-btn">
               <Icon name="bell" />
