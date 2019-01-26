aa46c65fc24e7db51dfd435406a917b0a9cf9c17
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2019-01-26 17:53:17 -0500

    handle clicks of different main menu sections

--- app/components/MainMenu/index.jsx
+++ app/components/MainMenu/index.jsx
@@ -6,22 +6,48 @@ import './style.scss'
 class MainMenu extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      headerMenuSelection: ['header_1'],
+      sectionMenuSelection: [],
+      footerMenuSelection: [],
+    }
+  }
+
+  manageSelections = event => {
+    let itemType = event.key.split('_')[0]
+
+    if (itemType === 'header') {
+      this.setState({
+        headerMenuSelection: [event.key],
+        sectionMenuSelection: [],
+        footerMenuSelection: [],
+      })
+    } else if (itemType === 'section') {
+      this.setState({
+        headerMenuSelection: [],
+        sectionMenuSelection: [event.key],
+        footerMenuSelection: [],
+      })
+    } else if (itemType === 'footer') {
+      this.setState({
+        headerMenuSelection: [],
+        sectionMenuSelection: [],
+        footerMenuSelection: [event.key],
+      })
+    }
   }
 
   render() {
+    const { headerMenuSelection, sectionMenuSelection, footerMenuSelection } = this.state
+
     return (
       <div className="main-menu">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['5']}
-          defaultOpenKeys={['5']}
-          style={{ ...this.props.style }}
-        >
-          <Menu.Item key="5">
+        <Menu mode="inline" selectedKeys={headerMenuSelection} style={{ ...this.props.style }}>
+          <Menu.Item key="header_1" onClick={this.manageSelections}>
             <Icon name="th-list" className="list-item-icon" />
             <span className="ant-menu-item__text">&nbsp; All Posts</span>
           </Menu.Item>
-          <Menu.Item key="6">
+          <Menu.Item key="header_2" onClick={this.manageSelections}>
             <span className="ant-menu-item__text">
               <Icon name="bolt" className="list-item-icon" />
               &nbsp; Must See
@@ -36,26 +62,23 @@ class MainMenu extends React.Component {
           </div>
         </div>
 
-        <Menu mode="inline" style={{ ...this.props.style }}>
-          <Menu.Item key="1">
+        <Menu mode="inline" style={{ ...this.props.style }} selectedKeys={sectionMenuSelection}>
+          <Menu.Item key="section_1" onClick={this.manageSelections}>
             <span className="ant-menu-item__text space-left--20">&nbsp; Design</span>
           </Menu.Item>
-          <Menu.Item key="2">
+          <Menu.Item key="section_2" onClick={this.manageSelections}>
             <span className="ant-menu-item__text space-left--20">&nbsp; Onboarding</span>
           </Menu.Item>
-          <Menu.Item key="3">
+          <Menu.Item key="section_3" onClick={this.manageSelections}>
             <span className="ant-menu-item__text space-left--20">&nbsp; Things to do</span>
           </Menu.Item>
         </Menu>
 
         <br />
 
-        <Menu mode="inline" style={{ ...this.props.style }}>
-          <Menu.Item key="7">
-            <span className="ant-menu-item__text">
-              <Icon name="edit" className="list-item-icon" />
-              &nbsp; Draft
-            </span>
+        <Menu mode="inline" style={{ ...this.props.style }} selectedKeys={footerMenuSelection}>
+          <Menu.Item key="footer_1" onClick={this.manageSelections}>
+            <span className="ant-menu-item__text space-left--20">&nbsp; Draft</span>
           </Menu.Item>
         </Menu>
       </div>
