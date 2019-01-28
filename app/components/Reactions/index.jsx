c96d0dbecc5edb1dfac743d557b311d17895109c
Rehaan <frankfill2007@gmail.com>
2019-01-27 21:53:45 -0500

    add reaction button

--- app/components/Reactions/index.jsx
+++ app/components/Reactions/index.jsx
@@ -8,17 +8,49 @@ import './style.scss'
 class Reactions extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      showPicker: false,
+    }
+  }
+
+  hidePicker = () => {
+    this.setState(
+      {
+        showPicker: false,
+      },
+      () => {
+        document.removeEventListener('click', this.hidePicker)
+      },
+    )
+  }
+
+  showPicker = event => {
+    event.preventDefault()
+    this.setState(
+      {
+        showPicker: !this.state.showPicker,
+      },
+      () => {
+        document.addEventListener('click', this.hidePicker)
+      },
+    )
   }
 
   render() {
+    const { showPicker } = this.state
     return (
       <div className="reactions-container space-left--15">
-        <div className="reactions-container__button">
+        <div className="reactions-container__button" onClick={this.showPicker}>
           <FontAwesomeIcon icon={['far', 'grin']} />
           <span> +</span>
         </div>
-        {/* <Picker emoji="point_up" /> */}
+        {showPicker && (
+          <Picker
+            style={{ marginLeft: '5px', boxShadow: '0px 2px 3px #00000014' }}
+            className="reactions-container__picker"
+            emoji="point_up"
+          />
+        )}
       </div>
     )
   }
